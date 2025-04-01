e4e501a2b0e1cd7d84c2f73ec728ee02151acd85
pdrlps1 <pedro@DESKTOP-JRJGPC2.>
2025-04-01 20:56:00 -0300

    feat: url with create function

---
+++ README.md
@@ -0,0 +1,8 @@
+**Para uso do Professor**
+
+ALUNOS:
+João Pedro Padovan Marques - 230122
+Pedro Otávio Lopes da Silva - 240070
+
+-   Rota para teste:
+    -   localhost/complaint/{nome}/{descrição}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,24 @@
 <?php
 
+use App\Models\Complaint;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/complaint/{resident}/{description}', function ($resident, $description) {
+    $date = now();
+
+    try {
+        Complaint::create([
+            'resident' => $resident,
+            'description' => $description,
+            'reported_at' => $date
+        ]);
+
+        return "Reclamação criada!";
+    } catch (Error) {
+        return "Falha na criação de uma reclamação!";
+    };
 });
